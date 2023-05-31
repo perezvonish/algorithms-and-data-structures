ebf66a4838ff2e624b733af38cabf9888086fd31
Vladimir <shokilak@gmail.com>
2023-05-31 21:48:41 +0600

    fix: DuplicateZeros

--- array/DuplicateZeros.js
+++ array/DuplicateZeros.js
@@ -1,17 +1,16 @@
-const arr = [1,0,2,3,0,4,5,0]
+const arr = [0,1,9,2,6,0,0,4,1,0]
 
 const duplicateZeros = function(arr) {
     const arrLength = arr.length
 
-    for (let i = 0; i < 8; i++){
-        if (arr[i] == 0){
-            for (let j = arr.length; j > i; j--){
-                arr[j+1] = arr[j]
+    for(let i=0; i < arr.length-1; i++){
+        if(arr[i] == 0){
+            for(let j= arr.length-1; j > i; j--){
+                arr[j] = arr[j-1];
             }
-
-            arr[i+1] = 0
-            i++
+            i++;
         }
+
     }
 
     for (let i = arr.length; i > arrLength; i--){
@@ -21,4 +20,4 @@ const duplicateZeros = function(arr) {
     return arr
 };
 
-duplicateZeros(arr)
+console.log(duplicateZeros(arr))
