b7f7e8dbb21491b4b9619fb2e761b5e47f2dabed
Vladimir <shokilak@gmail.com>
2023-06-01 12:36:16 +0600

    feat: Counter.

---
+++ array/Counter.js
@@ -0,0 +1,12 @@
+
+
+const createCounter = function(n) {
+    return function() {
+        return n++;
+    };
+};
+
+const counter = createCounter(10)
+counter() // 10
+counter() // 11
+counter() // 12
