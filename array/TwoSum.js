f46bf31a03ab9a899f88a52f74366096a1042756
Vladimir <shokilak@gmail.com>
2023-06-02 22:13:28 +0600

    feat: TwoSum on map

--- array/TwoSum.js
+++ array/TwoSum.js
@@ -1,14 +1,19 @@
-const nums = [2,7,11,15]
-const target = 9
-
 const twoSum = function(nums, target) {
+    const map = new Map()
+
     for (let i = 0; i < nums.length; i++){
-        for (let j = i + 1; j < nums.length; j++){
-            if (nums[i] + nums[j] == target){
-                return [i, j]
-            }
+        let result = target - nums[i]
+
+        if (map.has(result)){
+            return [map.get(result), i]
         }
+
+        map.set(nums[i], i)
     }
+
+    return []
 };
 
-console.log(twoSum(nums, target))
+console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([3,2,4], 6))
+console.log(twoSum([3,3], 6))
